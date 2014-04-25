721d3c8fb9f1f8e6723df9c65b43f86fc681a7f8
Hieu Phan <aznrice2k4@gmail.com>
2014-04-24 23:24:52 -0500

    Hacking for msm8660
    Change-Id: I4d07cc234558c83abebda024533f0fedb8d2fdd5
    Signed-off-by: Hieu Phan <aznrice2k4@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,6 +2,10 @@ ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8974 based platforms
     include $(call all-named-subdir-makefiles,msm8974)
 else
+ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
+    #This is for 8660 based platforms
+    include $(call all-named-subdir-makefiles,msm8960)
+else
 ifneq ($(filter msm8226,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8226 based platforms
     include $(call all-named-subdir-makefiles,msm8974)
@@ -10,4 +14,4 @@ else
     include $(call all-named-subdir-makefiles,msm8960)
 endif
 endif
-
+endif
